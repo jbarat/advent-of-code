b332ebd762016a8e3890be3d43c9b6bf7e48bfc3
Jozsef Barat <baratjozsef@gmail.com>
2017-12-05 19:20:03 +0000

    feat: day 2

---
+++ src/main/kotlin/year2017/day1/Day2.kt
@@ -0,0 +1,44 @@
+package year2017.day1
+
+class Day2 {
+    fun doIt(input: String): Int {
+        var sum = 0
+        input.split(delimiters = *charArrayOf('\n'))
+                .forEach {
+                    var min = Int.MAX_VALUE
+                    var max = Int.MIN_VALUE
+                    it.split(delimiters = *charArrayOf(' ', '\t'))
+                            .map { it.toInt() }
+                            .forEach {
+                                if (it < min) min = it
+                                if (it > max) max = it
+                            }
+
+                    sum += (max - min)
+                }
+
+        return sum
+    }
+
+    fun doItBetter(input: String): Int {
+        var sum = 0
+        input.split(delimiters = *charArrayOf('\n'))
+                .forEach {
+                    val sortedRow = it.split(delimiters = *charArrayOf(' ', '\t'))
+                            .map { it.toInt() }
+                            .sortedBy { it }
+
+                    sortedRow.forEach { current ->
+                        sortedRow.filter {
+                            it != current && it % current == 0
+                        }.map {
+                            sum += it / current
+                        }
+                    }
+
+                }
+
+        return sum
+    }
+
+}
