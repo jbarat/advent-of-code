f6817a816888ef219c08ea63ca2f169eb4d3f5fd
Jozsef Barat <baratjozsef@gmail.com>
2017-12-05 19:19:46 +0000

    feat: day 1 part 2

---
+++ src/main/kotlin/year2017/day1/Day1.kt
@@ -0,0 +1,34 @@
+package year2017.day1
+
+class Day1 {
+    fun doIt(input: String): Int {
+        if (input.length < 2) return 0
+
+        var sum = 0
+        val numbers = input.map { it.toString().toInt() }
+
+        val last = numbers[numbers.size - 1]
+        numbers.fold(last, { previous: Int, new: Int ->
+            if (previous == new) sum += previous
+            new
+        })
+
+        return sum
+    }
+
+    fun doItBetter(input: String): Int {
+        if (input.length < 2) return 0
+
+        var sum = 0
+        val pivot = input.length / 2
+        val numbers = input.map { it.toString().toInt() }
+
+        numbers.forEachIndexed { index: Int, i: Int ->
+            if (i == numbers[(index + pivot) % input.length]) {
+                sum += i
+            }
+        }
+
+        return sum
+    }
+}
